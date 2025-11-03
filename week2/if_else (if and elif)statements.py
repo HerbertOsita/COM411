fbb60f723fd979e595f5a4582c4496cc31fe4ac6
Herbert <0osits06@solent.ac.uk>
2025-11-03 13:44:54 +0000

    Added code to demonstrate how a variable can be used as

--- week2/if_else (if and elif)statements.py
+++ week2/if_else (if and elif)statements.py
@@ -36,3 +36,28 @@ else:
      print("The second number is the smallest.")
  else:
      print("The two numbers are equal.")
+
+# Ask user for numbers
+print("\nPlease enter the first whole number?")
+first_number = int(input())
+print("Please enter the second whole number?")
+second_number = int(input())
+print("Please enter the third whole number?")
+third_number = int(input())
+even_numbers = 0
+odd_numbers = 0
+# Determine which numbers are even and which are odd
+if first_number % 2 == 0:
+ even_numbers = even_numbers + 1
+else:
+ odd_numbers = odd_numbers + 1
+if second_number % 2 == 0:
+    even_numbers = even_numbers + 1
+else:
+ odd_numbers = odd_numbers + 1
+if third_number % 2 == 0:
+    even_numbers = even_numbers + 1
+else:
+    odd_numbers = odd_numbers + 1
+print(f"There were {even_numbers} even and {odd_numbers} odd numbers.")
+
