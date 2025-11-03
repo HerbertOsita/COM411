435debb90d193b29ba6edf1a988fc197af179c7b
Herbert <0osits06@solent.ac.uk>
2025-11-03 13:29:04 +0000

    Added code to demonstrate the use of comparison operators

--- week2/if_else (if and elif)statements.py
+++ week2/if_else (if and elif)statements.py
@@ -22,3 +22,17 @@ if number % 2 == 0:
  print(f"The number {number} is an even number.")
 else:
  print(f"The number {number} is an odd number.")
+
+
+ # Ask user for the direction
+ print("\nPlease enter the first number.")
+ first_number = int(input())
+ print("Please enter the second number.")
+ second_number = int(input())
+ # Determine which message to display
+ if first_number < second_number:
+     print("The first number is the smallest.")
+ elif first_number > second_number:
+     print("The second number is the smallest.")
+ else:
+     print("The two numbers are equal.")
