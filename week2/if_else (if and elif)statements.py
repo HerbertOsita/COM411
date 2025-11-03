f51774a2101db0211af5c5a0aae5e313d94b29d5
Herbert <0osits06@solent.ac.uk>
2025-11-03 12:26:03 +0000

    Added code to demonstrate the use of a modulus operator.

--- week2/if_else (if and elif)statements.py
+++ week2/if_else (if and elif)statements.py
@@ -10,4 +10,15 @@ if direction == "left":
     print(f"I am moving in a {direction} direction!")
 elif direction == "right":
     print(f"I am moving in a {direction} direction!")
+else:
+ print("\n Not sure which direction to move!")
 print()
+
+# Ask user for a number
+print("Please enter a whole number.")
+number = int(input())
+# Display relevant message
+if number % 2 == 0:
+ print(f"The number {number} is an even number.")
+else:
+ print(f"The number {number} is an odd number.")
