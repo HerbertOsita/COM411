c1cc689481993ca47380ae79a06e0650628c119d
Herbert <0osits06@solent.ac.uk>
2025-11-27 14:33:54 +0000

    added code

--- Week7/main.py
+++ Week7/main.py
@@ -1,10 +1,17 @@
 import csv
-
 import process
 import tui
 
 def read_data(file_path):
-    pass
+    tui.started(f"Reading data from {file_path}")
+    data = []
+    with open(file_path) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)
+        for line in csv_reader:
+            data.append(line)
+    tui.completed()
+    return data
 
 def run():
     athlete_data = read_data("athlete_events.csv")
@@ -12,15 +19,15 @@ def run():
     while True:
         selection = tui.menu()
         if selection == "years":
-            pass
+            process.list_years(athlete_data)
         elif selection == "tally":
-            pass
+            process.tally_medals(athlete_data)
         elif selection == "team":
-            pass
+            process.tally_team_medals(athlete_data)
         elif selection == "exit":
             break
         else:
             tui.error("Invalid Selection!")
 
-        if __name__ == "__main__":
-            run()
+if __name__ == "__main__":
+    run()
--- Week7/tui.py
+++ Week7/tui.py
@@ -1,33 +1,38 @@
-import csv
-import process
-import tui
+LINE_WIDTH = 85
 
-def read_data(file_path):
-    tui.started(f"Reading data from {file_path}")
-    data = []
-    with open(file_path) as csv_file:
-        csv_reader = csv.reader(csv_file)
-        next(csv_reader)
-        for line in csv_reader:
-            data.append(line)
-    tui.completed()
-    return data
 
-def run():
-    athlete_data = read_data("athlete_events.csv")
+def started(msg=""):
+    output = f"Operation started: {msg}..."
+    dashes = "-" * LINE_WIDTH
+    print(f"{dashes}\n{output}\n")
+def completed():
+    dashes = "-" * LINE_WIDTH
+    print(f"\nOperation completed.\n{dashes}\n")
 
-    while True:
-        selection = tui.menu()
-        if selection == "years":
-            process.list_years(athlete_data)
-        elif selection == "tally":
-            process.tally_medals(athlete_data)
-        elif selection == "team":
-            process.tally_team_medals(athlete_data)
-        elif selection == "exit":
-            break
-        else:
-            tui.error("Invalid Selection!")
+def error(msg):
+    print(f"Error! {msg}\n")
 
-if __name__ == "__main__":
-    run()
+def menu():
+    print(f"""Please select one of the following options:
+    {"[years]":<10} List unique years
+    {"[tally]":<10} Tally up medals
+    {"[team]":<10} Tally up medals for each team
+    {"[exit]":<10} Exit the program
+    """)
+    selection = input("Your selection: ")
+    return selection.strip().lower()
+
+def display_medal_tally(tally):
+ print(f"| {'Gold':<10} | {tally['Gold']:<10} |")
+ print(f"| {'Silver':<10} | {tally['Silver']:<10} |")
+ print(f"| {'Bronze':<10} | {tally['Bronze']:<10} |")
+
+def display_team_medal_tally(team_tally):
+    for team, tally in team_tally.items():
+        print(team)
+        print(f"\tGold:{tally['Gold']}, Silver:{tally['Silver']},Bronze:{tally['Bronze']}")
+
+def display_years(years):
+    sorted_years = sorted(years, reverse=True)
+    for year in sorted_years:
+        print(year)
